8cabf2aae8904861594ebd45769e50aa2f51f5ed
LVWood <leslee.wood@gmail.com>
2019-05-25 07:35:27 -0500

    .get and .post are functional

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -9,7 +10,24 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      activeSmurf: null
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  // Leslee: I moved addSmurf to App because I didn't have time to re-think any configuration different from what we did on Thursday with Friends and Trinkets.
+  addSmurf = (event, individual) => {
+    event.preventDefault();
+    axios 
+      .post("http://localhost:3333/smurfs", individual)
+      .then(res => this.setState({ activeSmurf: res.data }))
+      .catch(err => console.log(err))
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -17,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf} smurfs={this.state.smurfs} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,50 +1,73 @@
 import React, { Component } from 'react';
 
+
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      smurf: this.props.activeSmurf || {
+        name: '',
+        age: '',
+        height: ''
+      },     
     };
   }
 
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+  // Leslee: Moved all this to App. I can't rethink the config right now.
+  // addSmurf = (event, individual) => {
+  //   event.preventDefault();
+  //   axios 
+  //     .post("http://localhost:3333/smurfs", individual)
+  //     .then(res => this.setState({ activeSmurf: res.data }))
+  //     .catch(err => console.log(err))
+  //   this.setState({
+  //     name: '',
+  //     age: '',
+  //     height: ''
+  //   });
+  // }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    e.persist();
+    this.setState(prevState => ({ 
+      smurf: {
+        ...prevState.smurf,
+        [e.target.name]: e.target.value 
+      }
+    }));
   };
+
+  handleSubmit = e => {
+      this.props.addSmurf(e, this.state.smurf);
+      this.setState({
+          smurf: {
+              name: '',
+              age: '',
+              height: ''
+          }
+      })
+  }
 
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={this.handleSubmit}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.smurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.smurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.smurf.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
